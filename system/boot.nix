53dfd2d0017c57fd68d99e3dbdeda58ef79e8fb3
Ronald Mik <16256741+ronaldmik@users.noreply.github.com>
2025-07-12 23:05:46 +0200

    Prevent [drm] *ERROR* dc_dmub_srv_log_diagnostic_data: DMCUB error - collecting diagnostic data

--- system/boot.nix
+++ system/boot.nix
@@ -5,5 +5,9 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    kernelParams = [
+      # Prevent [drm] *ERROR* dc_dmub_srv_log_diagnostic_data: DMCUB error - collecting diagnostic data
+      "amdgpu.dcdebugmask=0x10"
+    ];
   };
 }
