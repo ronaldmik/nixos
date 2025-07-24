d98d0b9198997eab0c1178a822a2b2fe463288b6
Ronald Mik <16256741+ronaldmik@users.noreply.github.com>
2025-07-24 11:15:52 +0200

    Add logging to determine what liquidd did during boot

--- system/liquidctl.nix
+++ system/liquidctl.nix
@@ -20,6 +20,7 @@
       for i in /sys/class/hwmon/hwmon*/temp*_input; 
       do
         if [[ "$(<$(dirname $i)/name)" == "kraken2023elite" ]]; then 
+          echo 'Kraken found! Creating symlink to coolant temp for target [$i]'
           ln -sf $i /var/kraken_coolant_temp_input
           found=true
         fi
